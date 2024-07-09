1aec1af376c300e87b270fa13cd60c50c09425a7
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-07-09 22:26:58 +0300

    Add files via upload

---
+++ MOD-7_module_7_3.py
@@ -0,0 +1,38 @@
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names  # Convert the tuple to a list
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            with open(file_name, encoding='utf-8') as file:
+                for i, string in enumerate(file):
+                    low_line = string.lower()
+                    for char in low_line:
+                        if char in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                            low_line = low_line.replace(char, '')
+                            words = low_line.split()
+                            all_words[file_name] = words
+
+        return all_words
+    #
+    # def find(self, word):
+    #     found_positions = {}
+    #     all_words = self.get_all_words()
+    #     for file_name, words in all_words.items():
+    #         if word in words:
+    #             found_positions[file_name] = words.index(word) + 1
+    #     return found_positions
+    #
+    # def count(self, word):
+    #     counts = {}
+    #     all_words = self.get_all_words()
+    #     for file_name, words in all_words.items():
+    #         counts[file_name] = words.count(word)
+    #     return counts
+
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())  # Все слова
+# print(finder2.find('TEXT'))  # 3 слово по счёту
+# print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
